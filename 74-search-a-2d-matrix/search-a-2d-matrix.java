00242881245fbecf7ba338b023b764f00280a79d
Aritra Ghorai <aritraghorai879@gmail.com>
2026-08-14 19:28:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (80.41%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,23 +1,43 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix.length == 0)
-            return false;
 
-        int n = matrix.length;
-        int m = matrix[0].length;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
 
-        int low = 0;
-        int high = n * m - 1;
+        int lowRow = 0;
+        int highRow = rows - 1;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+        while (lowRow <= highRow) {
 
-            if (matrix[mid / m][mid % m] == target) {
-                return true;
-            } else if (matrix[mid / m][mid % m] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+            int midRow = lowRow + (highRow - lowRow) / 2;
+
+            if (target < matrix[midRow][0]) {
+                highRow = midRow - 1;
+            }
+            else if (target > matrix[midRow][cols - 1]) {
+                lowRow = midRow + 1;
+            }
+            else {
+                // Target can exist in this row
+                int low = 0;
+                int high = cols - 1;
+
+                while (low <= high) {
+
+                    int mid = low + (high - low) / 2;
+
+                    if (matrix[midRow][mid] == target) {
+                        return true;
+                    }
+                    else if (matrix[midRow][mid] < target) {
+                        low = mid + 1;
+                    }
+                    else {
+                        high = mid - 1;
+                    }
+                }
+
+                return false;
             }
         }
 
